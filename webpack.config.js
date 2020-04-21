2489d1276770b42396c0bb9a3eee546318cd5495
vae <xiaochangvv@gmail.com>
2020-04-21 18:02:57 +0800

    添加注释

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,14 @@ const webpack = require("webpack")
 
 module.exports = {
     mode: "development",
+    //入口
     entry:"./src/main.js",
+    //输出
     output: {
+        //输出文件名称 如果是代码拆分或者多个入口时
+        //filename: "[name].[hash].bundle.js"
         filename:"bundle.js",
+        //输出目录路径，应该是绝对路径
         path: path.resolve(__dirname,"./dist"),
     },
     module: {
