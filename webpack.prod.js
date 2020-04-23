5233e7ca82d551e5591b0e35a90cd8cbabb3ca35
vae <xiaochangvv@gmail.com>
2020-04-23 16:23:31 +0800

    prevent duplication

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,10 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
-    devtool: "source-map"
+    devtool: "source-map",
+    optimization: {
+        splitChunks:{
+            chunks: "all"
+        }
+    }
 });
